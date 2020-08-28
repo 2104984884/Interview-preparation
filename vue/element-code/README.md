dee68b7b925bbc283d80b5b46d8ab2463fcba00f
2104984884 <2104984884@qq.com>
2020-08-28 23:15:31 +0800

    coo

---
+++ vue/element-code/README.md
@@ -0,0 +1,6 @@
+pwa 渐进式网页应用 
+eslint  保证大家代码风格一致的 
+eslint 
+cssnext  autoprefixer
+
+
---
+++ vue/element-code/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+  @click="handleCLick"
+    class="el-button"
+    :class="[
+    type?'el-button--'+type:'',
+    {
+      'is-round':round,
+      'is-circle':circle,
+    }
+   ]"
+  >
+    <div>
+      <!-- {{type}} - {{tit}} -->
+      <slot></slot>
+    </div>
+  </button>
+</template>
+<script>
+// props 检查
+export default {
+  data() {
+    return {
+      tit: "hello",
+    };
+  },
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    round: Boolean,
+    circle:Boolean,
+  },
+  created() {
+    console.log(this.type, this.tit);
+  },
+  methods:{
+    handleCLick(e){
+      console.log(e)
+      this.$emit('click',e)
+    }
+  }
+};
+</script>
+<style>
+</style>
